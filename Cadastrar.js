1d7f9947395fa23985b34de2f267951d162e84fb
Manoel Ribeiro <43623951+ManoelRibeiro2018@users.noreply.github.com>
2023-09-12 20:40:25 -0300

    Add files via upload

---
+++ Cadastrar.js
@@ -0,0 +1,9 @@
+// Selecione o botão por seu ID
+var botao = document.getElementById("Voltar");
+
+// Adicione um ouvinte de evento de clique ao botão
+botao.addEventListener("click", function () {
+    // Redirecione para a outra página quando o botão for clicado
+    console.log("werwr");
+    window.location.href = "./Login.html";
+});
