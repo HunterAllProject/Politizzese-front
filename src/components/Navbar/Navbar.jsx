2f87ebdb4af9697e2c2c09f19a653435f452026b
JoseCMessias <josecmessias22@gmail.com>
2023-09-29 19:25:17 -0300

    subindo o Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,31 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import logoIMG from "../../assets/politizzeseLogo.svg";
+import perfilIMG from "../../assets/imgPerfil.svg";
+import "./Navbar.css";
+
 export const Navbar = () => {
     return (
-        <header>
-            <h1>Navbar</h1>
+        <header className="header" id="header">
+            <div className="imgLogoHeader">
+                <img src={logoIMG} alt="Imagem da logo" />
+
+                <ul>
+                    <li>
+                        <Link to="/servicos">Serviços</Link>
+                    </li>
+                    <li>
+                        <Link to="/pedidos">pedidos</Link>
+                    </li>
+                    <li>
+                        <Link to="/assinatiras">Assinaturas</Link>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="imgPerfilHeader">
+                <img src={perfilIMG} alt="Imagem do perfil" />
+            </div>
         </header>
     );
 };
