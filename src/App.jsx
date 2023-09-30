da8b67e2335000b190b227aa8f6276049562f9a5
JoseCMessias <josecmessias22@gmail.com>
2023-09-29 22:41:07 -0300

    Subindo página de login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar.jsx";
 import { Home } from "./pages/Home/Home.jsx";
 import { Footer } from "./components/Footer/Footer.jsx";
+import { Login } from "./pages/Login/Login.jsx";
 
 export const App = () => {
     return (
@@ -11,6 +12,7 @@ export const App = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                 </Routes>
+                <Login />
                 <Footer />
             </Router>
         </>
