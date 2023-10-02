1bd5ca4ef620a5f689ee396b5cc9b44d1ee65eaf
JoseCMessias <josecmessias22@gmail.com>
2023-10-02 20:24:22 -0300

    Subindo página de adicionar cartão

--- src/pages/AddCartao/AddCartao.jsx
+++ src/pages/AddCartao/AddCartao.jsx
@@ -1,35 +1,65 @@
 import * as React from "react";
+import { useState } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Cards from "react-credit-cards-2";
+import "react-credit-cards-2/dist/es/styles-compiled.css";
 import "./AddCartao.css";
-
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-// TODO remove, esta demonstração não precisa redefinir o tema.
 
 const defaultTheme = createTheme();
 
 export const AddCartao = () => {
+    const [state, setState] = useState({
+        number: "",
+        expiry: "",
+        cvc: "",
+        name: "",
+        focus: "",
+    });
+
+    const handleSubimit = (event) => {
+        event.preventDefault();
+    };
+
+    const handleInputChange = (evt) => {
+        const { name, value } = evt.target;
+
+        setState((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handleInputFocus = (evt) => {
+        setState((prev) => ({ ...prev, focus: evt.target.name }));
+    };
+
     return (
         <ThemeProvider theme={defaultTheme}>
             <Container component="main" maxWidth="xs">
                 <Box
                     sx={{
-                        marginTop: 8,
+                        marginTop: 15,
                         display: "flex",
                         flexDirection: "column",
                         alignItems: "center",
                     }}
                 >
+                    <Cards
+                        number={state.number}
+                        expiry={state.expiry}
+                        cvc={state.cvc}
+                        name={state.name}
+                        focused={state.focus}
+                    />
+
                     <Box
+                        onSubmit={handleSubimit}
                         component="form"
                         noValidate
-                        // onSubmit={handleSubmit}
-                        sx={{ mt: 10 }}
+                        sx={{ mt: 5 }}
                     >
                         <Grid container spacing={2}>
                             <Grid item xs={12}>
@@ -39,30 +69,40 @@ export const AddCartao = () => {
                                     id="number"
                                     label="Número do Cartão"
                                     name="number"
-                                    autoComplete="number"
+                                    type="text"
+                                    autoFocus
+                                    value={state.number}
+                                    onChange={handleInputChange}
+                                    onFocus={handleInputFocus}
                                 />
                             </Grid>
+
                             <Grid item xs={12} sm={6}>
                                 <TextField
                                     required
                                     fullWidth
-                                    id="validade"
+                                    id="expiry"
                                     label="MM / AA"
-                                    name="text"
-                                    autoComplete="validade"
+                                    name="expiry"
+                                    value={state.expiry}
+                                    onChange={handleInputChange}
+                                    onFocus={handleInputFocus}
                                 />
                             </Grid>
+
                             <Grid item xs={12} sm={6}>
                                 <TextField
-                                    autoComplete="given-name"
                                     name="cvc"
                                     required
                                     fullWidth
                                     id="codigo"
                                     label="CVC"
-                                    autoFocus
+                                    value={state.cvc}
+                                    onChange={handleInputChange}
+                                    onFocus={handleInputFocus}
                                 />
                             </Grid>
+
                             <Grid item xs={12}>
                                 <TextField
                                     required
@@ -71,7 +111,9 @@ export const AddCartao = () => {
                                     label="Nome do títular"
                                     type="text"
                                     id="name"
-                                    autoComplete="new-name"
+                                    value={state.name}
+                                    onChange={handleInputChange}
+                                    onFocus={handleInputFocus}
                                 />
                             </Grid>
                         </Grid>
@@ -81,7 +123,7 @@ export const AddCartao = () => {
                                 <p>Cancelar</p>
                             </Link>
 
-                            <Grid item xs={12}>
+                            <Grid item xs={10}>
                                 <Button
                                     type="submit"
                                     fullWidth
