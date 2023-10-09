2498fef54f3d150f11585f971a0739f2bd737eb2
JoseCMessias <josecmessias22@gmail.com>
2023-10-09 19:50:41 -0300

    Subindo a página de Extrato

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ import { RecSenha } from "./pages/RecSenha/RecSenha.jsx";
 import { AddCartao } from "./pages/AddCartao/AddCartao.jsx";
 import { ListarCartao } from "./pages/ListarCartao/ListarCartao.tsx";
 import { NovosServicos } from "./pages/NovosServicos/NovosServicos.jsx";
+<<<<<<< Updated upstream
 import {Assinaturas} from './pages/Assinaturas/Assinaturas.jsx';
+=======
+import { Assinaturas } from "./pages/Assinaturas/Assinaturas.jsx";
+import { Extrato } from "./pages/Extrato/Extrato.jsx";
+>>>>>>> Stashed changes
 
 export const App = () => {
     return (
@@ -14,6 +19,10 @@ export const App = () => {
             <Router>
                 <Routes>
                     <Route path="/" element={<Home />} />
+<<<<<<< Updated upstream
+=======
+                    <Route path="/login" element={<Login />} />
+>>>>>>> Stashed changes
                     <Route path="/cadastro" element={<Cadastro />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/recsenha" element={<RecSenha />} />
@@ -21,6 +30,10 @@ export const App = () => {
                     <Route path="/listarcartao" element={<ListarCartao />} />
                     <Route path="/novosservicos" element={<NovosServicos />} />
                     <Route path="/assinaturas" element={<Assinaturas />} />
+<<<<<<< Updated upstream
+=======
+                    <Route path="/extrato" element={<Extrato />} />
+>>>>>>> Stashed changes
                 </Routes>
             </Router>
         </>
