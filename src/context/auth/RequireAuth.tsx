fa453692b69a6deeb4b3a172c422f869af1e928f
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2023-10-11 17:21:15 -0300

    Corrigindo autenticação

--- src/context/auth/RequireAuth.tsx
+++ src/context/auth/RequireAuth.tsx
@@ -1,13 +1,13 @@
 import { AuthContext } from "./AuthContext";
-import {useContext} from "react";
+import { useContext } from "react";
 import React from "react";
-import { Home } from "../../pages/Home/Home";
+import { Login } from "../../pages/Login/Login";
 
 
 export const RequireAuth = ({ children }: { children: JSX.Element }) => {
     const auth = useContext(AuthContext);
-    if(auth.usuario){
-        return <Home/>;
+    if (!auth.usuario) {
+        return <Login />;
     }
     return children;
 }
