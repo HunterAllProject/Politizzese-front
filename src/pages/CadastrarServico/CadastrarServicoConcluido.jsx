4e66bbe6ba9a7b560edf967d383e0ce97188890e
JoseCMessias <josecmessias22@gmail.com>
2023-10-16 21:38:03 -0300

    subindo página de cadastrar serviço

---
+++ src/pages/CadastrarServico/CadastrarServicoConcluido.jsx
@@ -0,0 +1,83 @@
+import { Link } from "react-router-dom";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Navbar } from "../../components/Navbar/Navbar";
+import { Footer } from "../../components/Footer/Footer";
+import { Upload } from "../../components/Upload/Upload.jsx";
+import "./CadastrarServicoConcluido.css";
+
+export const CadastrarServicoConcluido = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="tituloAssinaturas">
+        <h1>Cadastrar Serviços</h1>
+        <div className="barraAssinaturas"></div>
+      </div>
+
+      <div className="containerCadastrarServicoConcluido">
+        <form className="cadastrarServicoFormConcluido">
+          <div className="cadastrarVervicoSubTituloConcluido">
+            <h1>DESCRIÇÃO DO SERVIÇO</h1>
+          </div>
+
+          <div className="descricaoCadastrarServicoConcluido">
+            <p>
+              Descreva brevemente o serviço que você deseja, 
+              para que nossos editores possam realizar o seu serviço.
+            </p>
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "40ch", color: "black" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <div>
+                <TextField
+                  id="outlined-textarea"
+                  label="Descreva o serviço"
+                  placeholder="Descrição"
+                  multiline
+                  type="text"
+                  required
+                />
+              </div>
+            </Box>
+          </div>
+
+          <div className="cadastrarVervicoSubTituloConcluido">
+            <h1>NOME DO SERVIÇO</h1>
+
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "40ch", color: "black" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <div>
+                <TextField
+                  id="outlined-textarea"
+                  label="Nome do serviço"
+                  placeholder="Nome do serviço"
+                  multiline
+                />
+              </div>
+            </Box>
+          </div>  
+
+          <div className="botaoCadastrarServicob">
+            <Link to="/listarcartao">
+              <button type="submit">PEDIDO CONCLUÍDO</button>
+            </Link>
+          </div>
+        </form>
+      </div>
+      <Footer />
+    </div>
+  );
+};
